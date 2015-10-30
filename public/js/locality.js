954b274b175c06b7a1a660a3a40a35c48d46fc4b
Manish Kumar Singh <manishkumarsingh@CFMAC107s-MacBook-Pro.local>
2015-10-30 21:29:08 +0530

    #0 basic folder

--- public/js/locality.js
+++ public/js/locality.js
@@ -1,33 +1,12 @@
 $(document).ready(function(){
 
-	$('#myCarousel').carousel({
-    pause: true,
-    interval: false
-})
-
-$('.carousel .item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-  
-  for (var i=0;i<6;i++) {
-    next=next.next();
-    if (!next.length) {
-    	next = $(this).siblings(':first');
-  	}
-    
-    next.children(':first-child').clone().appendTo($(this));
-  }
-});
-
-
-
-  $('#myCarousel .col-xs-125').click(function(){
-    $('#myCarousel .col-xs-125').removeClass('b');
-    $(this).addClass('b');
-
+	$('.multiseelctUl').on('click','li',function(){
+    $(this).remove();
   });
+
+  function appendTags(value){
+    $('.multiseelctUl').append('<li>'+value+'</li>');
+  }
+  appendTags('mains');
 
 });
